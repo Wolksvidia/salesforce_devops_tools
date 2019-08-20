773bdb0144210844255fdce63079582250602134
Emiliano Famá <wolksvidia@gmail.com>
2019-08-20 17:43:19 -0300

    working

--- deploy_especific_qasource/deploy_aura_lwc.py
+++ deploy_especific_qasource/deploy_aura_lwc.py
@@ -15,7 +15,7 @@ def retrive_and_extract(deploydir,orgname,outputdir='tmpdir'):
     shutil.rmtree(outputdir)
 
 
-def remove_cms_references(nddir):
+def remove_cms_references(nddir,namespace,new_namespace=None):
     """Remove CMS references from Aura and Classes"""
     aurapath = os.path.join(nddir,'aura')
     if os.path.exists(aurapath):
@@ -27,9 +27,12 @@ def remove_cms_references(nddir):
                 if '.cmp' in afile:
                     with open(os.path.join(cmppath,afile), 'r') as f:
                         dataf = f.read()
-                    if 'DiageoCMS:' in dataf:
+                    if namespace+':' in dataf:
                         print(afile)
-                        datan = dataf.replace('DiageoCMS:','c:')
+                        if new_namespace is None:
+                            datan = dataf.replace(namespace+':','c:')
+                        else:
+                            datan = dataf.replace(namespace+':',new_namespace+':')
                         os.remove(os.path.join(cmppath,afile))
                         with open(os.path.join(cmppath,afile), 'w') as f:
                             f.write(datan)
@@ -40,13 +43,24 @@ def remove_cms_references(nddir):
             if '-meta.xml' not in aclass:
                 with open(os.path.join(classpath,aclass),'r') as f:
                     dataf = f.read()
-                if 'DiageoCMS__' or 'DiageoCMS.' in dataf:
+                if namespace+'__' or namespace+'.' in dataf:
                     print(aclass)
-                    datan = dataf.replace('DiageoCMS__','')
-                    datam = datan.replace('DiageoCMS.','')
+                    if new_namespace is None:
+                        datan = dataf.replace(namespace+'__','')
+                        datam = datan.replace(namespace+'.','')
+                    else:
+                        datan = dataf.replace(namespace+'__',new_namespace+'__')
+                        datam = datan.replace(namespace+'.',new_namespace+'.')
                     os.remove(os.path.join(classpath,aclass))
                     with open(os.path.join(classpath,aclass), 'w') as f:
                         f.write(datam)
+            # else:
+            #     with open(os.path.join(classpath,aclass),'r') as f:
+            #         dataf = f.read()
+            #     with open(os.path.join(classpath,aclass), 'w') as f:
+            #         for line in dataf:
+            #             if 'packageVersions>' or '<minorNumber>' or '<majorNumber>' not in line:
+            #                 f.write(line)
 
 
 
@@ -89,5 +103,7 @@ def deploy_mdapi(source,orgname):
 
 if __name__ == '__main__':
     retrive_and_extract('src','edge5')
-    remove_cms_references('src')
+    remove_cms_references('src','DiageoCMS')
+    #remove_cms_references('src','DiageoCMS','CMSTESTQA')
     deploy_mdapi('src','QAsource')
+    #deploy_mdapi('src','TestCMSpackage')
