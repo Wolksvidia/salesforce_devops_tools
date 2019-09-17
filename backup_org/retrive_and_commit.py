b76c9a8ca76fbb6db649a571d984f02922039d15
Emiliano Famá <wolksvidia@gmail.com>
2019-09-17 16:37:19 -0300

    edit

--- backup_org/retrive_and_commit.py
+++ backup_org/retrive_and_commit.py
@@ -6,19 +6,19 @@ import datetime
 
 def retrive_and_extract(deploydir,orgname,outputdir='tmpdir'):
     """retrive metadata from org and extract metadata form zip file"""
-    shutil.rmtree(deploydir)
+    #shutil.rmtree(deploydir)
     os.mkdir(outputdir)
     cmd = 'sfdx force:mdapi:retrieve -r ' + outputdir + ' -u ' + orgname + ' -k package.xml'
     os.system(cmd)
     zfile = os.listdir(outputdir)[0]
     with zipfile.ZipFile(os.path.join(outputdir,zfile),'r') as datazip:
         datazip.extractall()
-    os.rename(zfile.split('.zip')[0],deploydir)
+    #os.rename(zfile.split('.zip')[0],deploydir)
     shutil.rmtree(outputdir)
 
 
 if __name__ == '__main__':
-    retrive_and_extract('src','edge5')
-    os.system('git add .')
-    os.system('git commit -m ' + datetime.datetime.today().isoformat())
-    os.system('git push origin master')
+    retrive_and_extract('unpackaged','edgeci')
+    #os.system('git add .')
+    #os.system('git commit -m ' + datetime.datetime.today().isoformat())
+    #os.system('git push origin master')
